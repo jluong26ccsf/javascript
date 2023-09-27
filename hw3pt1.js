3c03a973c8961fe856616a469ea02268f1750021
jluong26ccsf <142632981+jluong26ccsf@users.noreply.github.com>
2023-09-26 22:07:42 -0700

    Add files via upload

--- hw3pt1.js
+++ hw3pt1.js
@@ -1,5 +1,11 @@
-$(document).ready(function() {
-    $("myform").submit(function() {
-        alert("Submitted");
-        });
+
+$(document).ready(function (){
+    
+    $(function() {
+    $('button').on('click', function(event) {
+        event.preventDefault();
+        var hwag = $('input[type="number]').val();
+        console.log(hwag, );
     });
+});
+}
